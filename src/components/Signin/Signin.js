ff2f51cfe2e0d5f0ecf94a5556d51c23a22b00b0
vineethkumar12 <adepuvineethkumarvinni@gmail.com>
2023-01-22 12:38:57 +0530

    updated

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -22,7 +22,7 @@ export const Signin=({change,userdata})=> {
      e.preventDefault();
    
 
-      fetch('https://h-ziym.onrender.com/signin',{
+      fetch('https://h-ziyml.onrender.com/signin',{
         method:'post',
         headers:{'Content-Type':'application/json',
         "Access-Control-Allow-Origin": "*",
